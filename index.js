c3175727661626e5da6a3328b795692bde52c343
aniruddha <ani@dctacademy.com>
2023-09-06 12:51:04 +0530

    with router

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express()
 const port = process.env.PORT || 3030
 
 const configureDB = require('./config/db')
+const routes = require('./config/routes')
 configureDB()
+app.use(express.json())
+app.use(cors()) 
+app.use(routes)
+
 
 app.listen(port, () => {
     console.log('server running on port', port)
